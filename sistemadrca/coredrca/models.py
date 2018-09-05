148d06c9784c5cbfe759ee642c0091acd6591583
Gabriela Almeida <g.almeida9196@yahoo.com.br>
2018-09-04 23:54:50 -0300

    Ajustes

--- sistemadrca/coredrca/models.py
+++ sistemadrca/coredrca/models.py
@@ -9,22 +9,41 @@ class Credito(models.Model):
     a_credito_o = models.IntegerField()
     #num de creditos não obrigatorios do aluno
     a_credito_l = models.IntegerField()
+    def __str__(self):
+        return self.d_credito
+
 
 class Departamento(models.Model):
     nome = models.CharField(max_length = 30)
 
+    def __str__(self):
+        return self.nome
+
+
 class Professor(models.Model):
     nome = models.CharField(max_length = 30)
     departamento = models.ForeignKey(Departamento, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.nome
+
 
 class Secretaria(models.Model):
     nome = models.CharField(max_length = 30)
     tipo = models.IntegerField()
     departamento = models.ForeignKey(Departamento, on_delete=models.PROTECT, null=True)
 
+    def __str__(self):
+        return self.nome
+
+
 class Curso(models.Model):
     nome = models.CharField(max_length = 30)
     secretaria = models.ForeignKey(Secretaria, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.nome
+
 
 class Disciplina(models.Model):
     nome = models.CharField(max_length = 30)
@@ -36,8 +55,16 @@ class Disciplina(models.Model):
     d_requisito = models.ManyToManyField('Disciplina')
     professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.nome
+
+
 class Aluno(models.Model):
     nome = models.CharField(max_length = 30)
     matricula = models.IntegerField()
+    curso = models.ForeignKey(Curso, on_delete=models.PROTECT, null = True)
     credito = models.ForeignKey(Credito, on_delete=models.PROTECT)
-    disciplinas = models.ManyToManyField(Disciplina)
+    disciplinas = models.ManyToManyField(Disciplina, blank=True)
+
+    def __str__(self):
+        return self.nome
