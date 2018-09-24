55514618ba184690954b22fa2c801edc64d46d7d
Gabriela Almeida <g.almeida9196@yahoo.com.br>
2018-09-24 00:11:09 -0300

    fontawesome

--- sistemadrca/coredrca/views.py
+++ sistemadrca/coredrca/views.py
@@ -3,10 +3,11 @@ from django.http.response import HttpResponse
 from django.template import RequestContext
 from coredrca.models import Aluno
 
+
 def home(request):
-    alunos = Aluno.objects.all()
     return render(request, "index.html")
 
-def alunos(request):
-    alunos = Aluno.objects.all()
-    return render(request, "alunos.html", {'alunos':alunos})
+def alunos(request, ):
+    alunos = Aluno.objects.all() 
+    context = {'alunos':alunos}
+    return render(request, "alunos.html", context)
--- sistemadrca/sistemadrca/settings.py
+++ sistemadrca/sistemadrca/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bootstrap4',
-    'fontawesome',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'coredrca',
+    'fontawesome',
 ]
 
 MIDDLEWARE = [
