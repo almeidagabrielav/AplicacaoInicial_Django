c12c2499005cb5732a3a109b95c2887fe6b09683
Gabriela Almeida <g.almeida9196@yahoo.com.br>
2018-09-25 18:10:25 -0300

    Form Novo Aluno

--- sistemadrca/coredrca/forms.py
+++ sistemadrca/coredrca/forms.py
@@ -5,3 +5,7 @@ class ProductForm(form.ModelForm):
     class Meta:
         model = Aluno
         fields = ['nome', 'matricula', 'curso', 'credito', 'disciplinas']
+
+class AlunoForm(forms.Form):
+    nomeAtual = forms.CharField(max_length=100)
+    matriculaAtual = forms.CharField(max_length=50)
--- sistemadrca/coredrca/views.py
+++ sistemadrca/coredrca/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 from django.template import RequestContext
-from coredrca.models import Aluno
+from coredrca.models import Aluno, Curso, Disciplina
+from .forms import AlunoForm
 
 
 def home(request):
     return render(request, "index.html")
 
 def alunos(request):
-    alunos = Aluno.objects.all() 
+    alunos = Aluno.objects.all()
     context = {'alunos':alunos}
     return render(request, "alunos.html", context)
 
 def criarAluno(request):
     form = ProductForm(request.POST or None)
+    cursos = Curso.objects.all()
+    disciplinas = Disciplina.objects.all()
 
     if form.is_valid():
         form.save()
         return redirect('alunos')
     
     return render(request, 'modalNovoAluno.html', {'form':form})
+
+def salvarAluno(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.
